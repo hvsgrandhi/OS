6ecf377f0fe52608833eff3e27d6c3c2bd54485c
Harshvardhan Grandhi <97739686+hvsgrandhi@users.noreply.github.com>
2023-11-03 07:42:23 +0530

    4b.c
    available

--- Assg4(Producer-consumer n ReaderWriter)/4b.c
+++ Assg4(Producer-consumer n ReaderWriter)/4b.c
@@ -103,3 +103,20 @@ void *reader(void *argp)
 // Element read by reader=75
 // Enter the no to add in buffer : ^C
 // comp123@comp123:~/Desktop/os/4$
+
+
+/*The Reader-Writer Problem is a synchronization challenge in operating systems and concurrent programming where multiple processes need to access a shared resource, but with different access requirements. The problem typically involves readers who can read the resource simultaneously and writers who have exclusive write access. Here's how it works:
+
+1. Readers: Multiple reader processes can access the shared resource concurrently without any issues, as reading does not modify the resource.
+
+2. Writers: Writer processes, on the other hand, need exclusive access to the resource, as they intend to modify it. This means only one writer can access the resource at a time, and no other readers or writers can access it during that time.
+
+3. Synchronization: To manage access to the shared resource, synchronization mechanisms like semaphores, mutexes, or other concurrency control tools are employed.
+
+4. Priority: The Reader-Writer Problem can be implemented with different priorities. For example, a reader-preference approach allows readers to proceed unless a writer is waiting. A writer-preference approach allows waiting writers to gain access as soon as the resource becomes available, even if there are waiting readers.
+
+5. Starvation: Care must be taken to prevent issues like writer starvation, where writers may be waiting indefinitely if readers are continuously accessing the resource.
+
+6. Balance: Achieving a balance between the needs of readers and writers, ensuring data integrity, and preventing race conditions is the key challenge in solving the Reader-Writer Problem.
+
+The Reader-Writer Problem highlights the need for careful synchronization and resource management in a concurrent system. The goal is to allow readers to access the resource concurrently while preventing any conflicts or data corruption when writers modify the resource. Implementations of the Reader-Writer Problem can vary based on the specific requirements and priorities of the system.*/
